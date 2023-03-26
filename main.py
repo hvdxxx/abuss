3605596dabb298ee4d5dc848e20d8f9b74cf5a8d
hvdxxx <retjarhan123@gmail.com>
2023-03-26 22:35:32 +0900

    hvdxxx 22:35

--- main.py
+++ main.py
@@ -4,35 +4,40 @@ import io
 from PIL import Image, ImageDraw, ImageFont
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.types import ReplyKeyboardMarkup
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from dotenv import load_dotenv
 
 load_dotenv()
+storage = MemoryStorage()
 bot = Bot(os.getenv('TOKEN'))
-dp = Dispatcher(bot=bot)
+dp = Dispatcher(bot = bot, storage = storage)
 
 main = ReplyKeyboardMarkup(resize_keyboard=True)
 main.add('Ввести ФИО водителя').add('Инфо').add('Контакт')
 
-@dp.message_handler(content_types=types.ContentTypes.TEXT)
-async def echo_photo(message: types.Message):
-    # загружаем изображение из файла
-    await message.answer(f'Отправлено2.')
+#abuss
+@dp.message_handler(text='Ввести ФИО водителя')
+async def start_fio_handler(message: types.Message):
+    await message.answer('Жду ФИО водителя.')
+    # Сохраняем флаг ожидания ФИО водителя для данного пользователя
+    context = dp.current_state(chat=message.chat.id)
+    await context.set_state('waiting_for_fio')
+
+# Вложенный обработчик текстовых сообщений
+@dp.message_handler(state='waiting_for_fio')
+async def handle_text_messages(message: types.Message):
     with Image.open('Images/Image.jpg') as img:
-        # добавляем текст на изображение
-        user_text = message.text
+        # Добавляем ФИО водителя на изображение
         draw = ImageDraw.Draw(img)
-        font = ImageFont.truetype("arial.ttf", 50)  # выбираем шрифт и его размер
-        draw.text((50, 50), user_text, (255, 255, 255), font=font)  # добавляем текст на изображение
-        img.save('Images/output.jpg')  # сохраняем изображение с текстом
-        # отправляем пользователю изображение с текстом
+        font = ImageFont.truetype("arial.ttf", 50)
+        draw.text((50, 50), message.text, (255, 255, 255), font=font)
+        img.save('Images/output.jpg')
+        # Отправляем пользователю изображение с ФИО водителя
         with open('Images/output.jpg', 'rb') as photo:
             await bot.send_photo(message.chat.id, photo)
-            await message.answer(f'Отправлено2.')
-
-#abuss
-@dp.message_handler(text=['Ввести ФИО водителя'])
-async def cmd_start(message: types.Message):
-    await message.answer(f'Жду ФИО водителя.')
+    # Сбрасываем флаг ожидания ФИО водителя для данного пользователя
+    context = dp.current_state(chat=message.chat.id)
+    await context.reset_state()
 
 
 #FIO
